3d7fac6d8e0c1daa9326ccd08c860862b680b199
Leonardo Almeida <leoalmeida.pessoal@gmail.com>
2023-09-29 16:25:41 -0300

    CHORE:components

--- src/components/login/LoginForm.vue
+++ src/components/login/LoginForm.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="col-md-4 d-flex flex-column justify-content-center align-items-center bg-primary text-white">
+  <div
+    class="col-md-4 d-flex flex-column justify-content-center align-items-center bg-primary text-white"
+  >
     <div class="row">
       <div class="col-12 mb-4">
         <h4>Bem-vindo !</h4>
@@ -7,32 +9,55 @@
     </div>
     <div class="row">
       <form ref="form" @submit.prevent="submitForm">
-        <TheInputEmail ref="un" v-model="object.email" divClass="col-12" label="E-mail" placeholder="E-mail" />
-        <TheInputPassword ref="pw" v-model="object.password" divClass="col-12" label="Senha" placeholder="Senha" @enter="login"/>
+        <TheInputEmail
+          ref="un"
+          v-model="object.email"
+          divClass="col-12"
+          label="E-mail"
+          placeholder="E-mail"
+        />
+        <TheInputPassword
+          ref="pw"
+          v-model="object.password"
+          divClass="col-12"
+          label="Senha"
+          placeholder="Senha"
+          @enter="login"
+        />
       </form>
     </div>
     <hr />
     <div class="row">
       <div class="col-12">
-        <TheButton label="Entrar" color="light" type="submit" icon="box-arrow-in-right" @click="login()" />
+        <TheButton
+          label="Entrar"
+          color="light"
+          type="submit"
+          icon="box-arrow-in-right"
+          @click="login()"
+        />
       </div>
     </div>
-    <TheModalError ref="modalMessage" modalTitle="Falha ao conectar!" :modalBody="modalBody" />
+    <TheModalError
+      ref="modalMessage"
+      modalTitle="Falha ao conectar!"
+      :modalBody="modalBody"
+    />
   </div>
 </template>
 
 <script>
-import axios from 'axios'
-import { Modal } from 'bootstrap'
-import { get } from '@/crud.js'
-import { baseApiUrl } from '@/global'
-import TheInputEmail from '@/components/formComponents/TheInputEmail.vue'
-import TheInputPassword from '@/components/formComponents/TheInputPassword.vue'
-import TheButton from '@/components/formComponents/TheButton.vue'
-import TheModalError from '../formComponents/TheModalError.vue'
+import axios from "axios";
+import { Modal } from "bootstrap";
+import { get } from "@/crud.js";
+import { baseApiUrl } from "@/global";
+import TheInputEmail from "@/components/formComponents/TheInputEmail.vue";
+import TheInputPassword from "@/components/formComponents/TheInputPassword.vue";
+import TheButton from "@/components/formComponents/TheButton.vue";
+import TheModalError from "../formComponents/TheModalError.vue";
 
 export default {
-  name: 'Login',
+  name: "Login",
 
   components: {
     TheInputEmail,
@@ -51,51 +76,57 @@ export default {
   methods: {
     async login() {
       if (!this.object.password || !this.object.email) {
-        this.modalBody = 'Informe o Usuário e a Senha de acesso.'
-        this.modalMessage.show()
+        this.modalBody = "Informe o Usuário e a Senha de acesso.";
+        this.modalMessage.show();
       } else {
-        const url = `${baseApiUrl}/login`
-        axios.defaults.withCredentials = true
+        const url = `${baseApiUrl}/login`;
+        axios.defaults.withCredentials = true;
 
         await axios
           .post(url, this.object)
           .then(async (res) => {
-            const rawUser = await get('me')
-            this.$store.dispatch('setLogged', true)
-            this.$store.dispatch('setUser', rawUser)
-            this.$router.push({ name: 'home' })
-            return true
+            const rawUser = await get("me");
+            this.$store.dispatch("setLogged", true);
+            this.$store.dispatch("setUser", rawUser);
+            this.$router.push({ name: "home" });
+            return true;
           })
           .catch((err) => {
-            if(err.response.status == 401) {
-              this.modalBody = 'Usuário ou senha incorretos. Por favor, verifique.'
-              this.modalMessage.show()
+            if (err.response.status == 401) {
+              this.modalBody =
+                "Usuário ou senha incorretos. Por favor, verifique.";
+              this.modalMessage.show();
             } else if (err.response.status == 400) {
-              this.modalBody = 'Usuário ou senha não informados. Por favor, verifique.'
-              this.modalMessage.show()
+              this.modalBody =
+                "Usuário ou senha não informados. Por favor, verifique.";
+              this.modalMessage.show();
             } else if (err.response.status == 403) {
-              this.modalBody = 'Usuário inativo.'
-              this.modalMessage.show()
+              this.modalBody = "Usuário inativo.";
+              this.modalMessage.show();
             }
-          })
+          });
       }
     },
 
     interpretador(param) {
-      eval(`this.${param}()`)
+      eval(`this.${param}()`);
     },
   },
 
   mounted() {
-    this.modalMessage = new Modal(this.$refs.modalMessage.$refs.modalPattern)
+    this.modalMessage = new Modal(this.$refs.modalMessage.$refs.modalPattern);
   },
-}
+};
 </script>
 
 <style>
 .bg {
   animation: slide 3s ease-in-out infinite alternate;
-  background-image: linear-gradient(-20deg, rgb(87, 140, 255) 50%, rgb(101, 171, 236) 50%);
+  background-image: linear-gradient(
+    -20deg,
+    rgb(87, 140, 255) 50%,
+    rgb(101, 171, 236) 50%
+  );
   bottom: 0;
   left: -100%;
   opacity: 0.5;
@@ -133,4 +164,5 @@ export default {
 .iconButton {
   cursor: pointer;
 }
+
 </style>
